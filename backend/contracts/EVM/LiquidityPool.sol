18a1589d71ea38addd7b0cf0b580c0a2f230dd84
Edu <emailparaoedu@gmail.com>
2024-07-13 13:20:58 +0100

    almost done contracts

---
+++ backend/contracts/EVM/LiquidityPool.sol
@@ -0,0 +1,89 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+
+pragma solidity >=0.8.13;
+
+import "../Hyperlane/Common.sol";
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IMintableEncryptedERC20 {
+    function mintAndDepositPool(uint256 amount, address token, address darkPool, address to) external;
+
+    function burnAndWithdrawPool(uint256 amount, address token, address darkPool, address to) external;
+}
+
+/*
+	Contract deployed on base Sepolia
+*/
+contract LiquidityPool is Ownable, BridgeContract {
+    // allowed tokens?
+    // mapping(address => bool) public allowedTokens;
+
+    function setAllowedToken(address token, bool allowed) public onlyOwner {
+        // allowedTokens[token] = allowed;
+    }
+
+    address public darkPoolAddress;
+
+    function setDarkPoolAddress(address _darkPoolAddress) public onlyOwner {
+        darkPoolAddress = _darkPoolAddress;
+    }
+
+    // deposits the ERC20 token into the EVM contract and then calls FHEVM to adds the mirrored token to the user's account
+    function depositAndPortToken(address token, uint256 amount) external {
+        // require(allowedTokens[token], "LiquidityPool: token not allowed");
+        require(IERC20(token).allowance(msg.sender, address(this)) >= amount, "LiquidityPool: insufficient allowance");
+        require(darkPoolAddress != address(0), "LiquidityPool: darkPoolAddress not set");
+
+        // call FHEVM to add the mirrored token to the user's account
+        IMintableEncryptedERC20 _MintableERC20 = IMintableEncryptedERC20(mirroredERC20Target);
+
+        bytes memory _callback = abi.encodePacked(this.callbackPortMinted.selector, (uint256(uint160(msg.sender))));
+
+        bytes32 messageId = IInterchainExecuteRouter(iexRouter).callRemote(
+            DestinationDomain,
+            address(_MintableERC20),
+            0,
+            abi.encodeWithSignature(
+                "mintAndDepositPool(uint256,address,address,address)",
+                amount,
+                token,
+                darkPoolAddress,
+                msg.sender
+            ),
+            _callback
+        );
+    }
+
+    // redeems the ERC20 token from the EVM contract and then calls FHEVM to burn the mirrored token from the user's account
+    function redeemTokenAndBurn(address token, uint256 amount) external {
+        // require(allowedTokens[token], "LiquidityPool: token not allowed");
+        require(IERC20(token).balanceOf(address(this)) >= amount, "LiquidityPool: insufficient balance");
+
+        // call FHEVM to add the mirrored token to the user's account
+        IMintableEncryptedERC20 _MintableERC20 = IMintableEncryptedERC20(mirroredERC20Target);
+
+        bytes memory _callback = abi.encodePacked(this.callbackPortBurnt.selector, (uint256(uint160(msg.sender))));
+
+        bytes32 messageId = IInterchainExecuteRouter(iexRouter).callRemote(
+            DestinationDomain,
+            address(_MintableERC20),
+            0,
+            abi.encodeCall(_MintableERC20.burnAndWithdrawPool, (amount, token, darkPoolAddress, msg.sender)),
+            _callback
+        );
+
+        //TODO: verify if the withdraw was made from the id sucessfully? if so transfer the token to the user
+        // TODO: or assume if it didn't revert everthing went well?
+    }
+
+    function callbackPortMinted() external {
+        require(caller_contract == msg.sender, "not right caller contract");
+    }
+
+    function callbackPortBurnt(address user, uint8 amount, address token) external {
+        require(caller_contract == msg.sender, "not right caller contract");
+        IERC20(token).transfer(user, amount);
+    }
+}
