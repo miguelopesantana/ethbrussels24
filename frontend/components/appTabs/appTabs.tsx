a8a78c039dac4e999f96ada5eb516556e83f6e60
Edu <emailparaoedu@gmail.com>
2024-07-14 06:35:44 +0100

    removed useless menu

--- frontend/components/appTabs/appTabs.tsx
+++ frontend/components/appTabs/appTabs.tsx
@@ -215,29 +215,6 @@ export default function AppTabs() {
                     </CardFooter>
                 </Card>
             </TabsContent>
-            <TabsContent value="pool" className="flex-1 flex justify-center items-center data-[state=inactive]:hidden">
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Password</CardTitle>
-                        <CardDescription>
-                            Change your password here. After saving, you'll be logged out.
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="current">Current password</Label>
-                            <Input id="current" type="password" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="new">New password</Label>
-                            <Input id="new" type="password" />
-                        </div>
-                    </CardContent>
-                    <CardFooter>
-                        <Button>Save password</Button>
-                    </CardFooter>
-                </Card>
-            </TabsContent>
         </Tabs>
     )
 }
